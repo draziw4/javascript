206a6f3693639cf661f0038242883547564f4cc7
Filipi Sousa <filipisousasilva6@gmail.com>
2024-03-27 18:53:22 -0300

    javascript

---
+++ aula11/ex008.js
@@ -0,0 +1,11 @@
+var vel = 60.5
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60){
+    console.log(`Você foi multado`)
+}
+else {
+    console.log(`Você está dentro da velocidade permitida`)
+}
+
